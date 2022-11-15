dc5cc053754df62e017bc6ae24f8678a6b0383f2
Justin D <isaac_justin@hotmail.com>
2022-11-15 15:33:27 -0600

    practice routing

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <button @click="$router.push('/products')">Go to products</button>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>product PID: {{ $route.params.pid }}</h1>
+</template>
